30e8bba06b94c21c41d19959e023b959b625ce5a
Pawel Blazejewski <pblazh@gmail.com>
2025-04-25 18:06:11 +0200

    #02: Parse command line parameters

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,12 +1,48 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
+	"strings"
 )
 
-var usageMessage = "Usage: 'csvss -s ./script.file ./table.csv'"
+var (
+	usageMessage      = "Usage: 'csvss -s ./script.file ./table.csv'"
+	csvPathMessage    = "Path to a csv file is required"
+	scriptPathMessage = "Path to a script file is required"
+)
 
 func main() {
-	_, _ = os.Stdout.WriteString(usageMessage + "\n")
-	os.Exit(0)
+	var script string
+	var inPlace bool
+	var help bool
+
+	flag.StringVar(&script, "s", "", "path to a script file")
+	flag.BoolVar(&inPlace, "i", false, "update CSV file in place")
+	flag.BoolVar(&help, "h", false, "usage")
+	flag.Parse()
+
+	if help {
+		_, _ = os.Stdout.WriteString(usageMessage + "\n")
+		os.Exit(0)
+	}
+
+	args := flag.Args()
+
+	if len(args) == 0 {
+		_, _ = os.Stderr.WriteString(
+			strings.Join([]string{csvPathMessage, usageMessage, ""}, "\n"),
+		)
+		os.Exit(1)
+	}
+
+	if script == "" {
+		_, _ = os.Stderr.WriteString(
+			strings.Join([]string{scriptPathMessage, usageMessage, ""}, "\n"),
+		)
+		os.Exit(1)
+	}
+
+	fmt.Println(script)
 }
--- tpm.md
+++ tpm.md
@@ -1,5 +1,5 @@
 # CSV spreadsheet
 
 - [x] #01: Scaffold a project
-- [ ] #02: Parse command line parameters
+- [x] #02: Parse command line parameters
 - [ ] #03: Read CSV file into memory
