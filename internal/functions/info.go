4db4649c35b5fba0c5a4bb19f4ef87601a9a8b20
Pawel Blazejewski <pblazh@gmail.com>
2025-08-16 10:07:01 +0200

    #39.4: Implement ISLOGICAL function

--- internal/functions/info.go
+++ internal/functions/info.go
@@ -21,3 +21,12 @@ func IsText(format string, call ast.CallExpression, values ...ast.Expression) (a
 
 	return ast.BooleanExpression{Value: ast.IsString(values[0]), Token: call.Token}, nil
 }
+
+func IsBoolean(format string, call ast.CallExpression, values ...ast.Expression) (ast.Expression, error) {
+	callGuard := MakeArityGuard(format, 1)
+	if err := callGuard(call, values...); err != nil {
+		return nil, err
+	}
+
+	return ast.BooleanExpression{Value: ast.IsBoolean(values[0]), Token: call.Token}, nil
+}
