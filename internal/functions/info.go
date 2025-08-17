ac12b013618ddafca57086ec78dd1cec620982e7
Pawel Blazejewski <pblazh@gmail.com>
2025-08-17 19:40:13 +0200

    #39.1: Implement ISBLANK function (#27)

--- internal/functions/info.go
+++ internal/functions/info.go
@@ -30,3 +30,16 @@ func IsBoolean(format string, call ast.CallExpression, values ...ast.Expression)
 
 	return ast.BooleanExpression{Value: ast.IsBoolean(values[0]), Token: call.Token}, nil
 }
+
+func IsBlank(format string, call ast.CallExpression, values ...ast.Expression) (ast.Expression, error) {
+	callGuard := MakeArityGuard(format, 1)
+	if err := callGuard(call, values...); err != nil {
+		return nil, err
+	}
+	switch v := values[-0].(type) {
+	case ast.StringExpression:
+		return ast.BooleanExpression{Value: v.Value == "", Token: call.Token}, nil
+	default:
+		return ast.BooleanExpression{Value: false, Token: call.Token}, nil
+	}
+}
